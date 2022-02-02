89f9515a728e528475bf412102a7e06151a6bc34
odmfl <77236251+odmfl@users.noreply.github.com>
2022-02-02 12:47:03 +0100

    Update DataModel.java

--- app/src/main/java/com/android/deskclock/data/DataModel.java
+++ app/src/main/java/com/android/deskclock/data/DataModel.java
@@ -723,6 +723,15 @@ public final class DataModel {
         enforceMainLooper();
         return mAlarmModel.getAlarmVolumeButtonBehavior();
     }
+    
+     /**
+     * @return the behavior to execute when power buttons are pressed while firing an alarm
+     */
+    public AlarmVolumeButtonBehavior getAlarmPowerButtonBehavior() {
+        enforceMainLooper();
+        return mAlarmModel.getAlarmPowerButtonBehavior();
+    }
+
 
     /**
      * @return the number of minutes an alarm may ring before it has timed out and becomes missed
